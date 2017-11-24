9db3dcd3040ca8a2d48ad83d5dac5cbe8703a4ae
= <cortical.iv@gmail.com>
2017-11-23 23:40:15 -0500

    Updating settings

--- restit/settings.py
+++ restit/settings.py
@@ -122,8 +122,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 STATIC_URL = '/static/'
+
+#Static asset configuration [new from https://devcenter.heroku.com/articles/django-assets]
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static') #'staticfiles')
+STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+        )
 
 #Heroku settings
 cwd = os.getcwd()
